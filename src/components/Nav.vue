ac814691aa91eb651d5854efca9b00c9a68b7601
tw_zgn <672236157@qq.com>
2022-05-28 23:06:54 +0800

    money 页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav">
     <!-- vue router提供的 router-link标签用于切换页面作用和a标签一样 -->
     <router-link to="/money">记账</router-link>
     --
@@ -16,4 +16,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// scoped 作用是限制特template中的class属性仅在本组件有效
+.nav {
+  border: 1px solid red;
+}
 </style>
