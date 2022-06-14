74d4c97a6a89e3af7a03607578b40fd629e71274
tai-xiaoDong <672236157@qq.com>
2022-06-15 01:40:15 +0800

    Tags 组件完成，可以添加tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li
@@ -21,16 +21,24 @@ import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 
 @Component
-export default class Types extends Vue {
-  @Prop(Array) dataSource: string[] | undefined;
+export default class Tags extends Vue {
+  @Prop() readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
-    if (this.selectedTags.indexOf(tag) >= 0) {
+    if (index >= 0) {
       this.selectedTags.splice(index, 1);
     } else {
       this.selectedTags.push(tag);
+    }
+  }
+  create() {
+    const name = window.prompt("请输入标签名");
+    if (name === "") {
+      window.alert("标签名不能为空！");
+    } else if (this.dataSource) {
+      this.$emit("update:dataSource", [...this.dataSource, name]);
     }
   }
 }
