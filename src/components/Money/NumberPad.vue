553f2ad19e834664eaac452ab998108769039f44
tai-xiaoDong <672236157@qq.com>
2022-06-16 07:41:47 +0800

    添加一些备注

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -29,7 +29,7 @@ export default class NumberPad extends Vue {
   output = "0"; //页面默认显示为0
 
   inputContent(event: MouseEvent) {
-    const button = event.target as HTMLButtonElement; //强制指定类型
+    const button = event.target as HTMLButtonElement; //强制指定类型,ts断言
     const input = button.textContent!;
     if (this.output.length === 16) {
       return;
