9ca50c992d501ff3fee9e9cc75d119ff4e32ed0f
tai-xiaoDong <672236157@qq.com>
2022-06-27 13:54:15 +0800

    修改了一个变量名

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="form-wrapper">
       <FormItem
-        :value="tag.name"
+        :value="currentTag.name"
         @update:value="update"
         fieldName="标签名"
         placeholder="请在此处输入标签名"
@@ -29,25 +29,25 @@ import Dbutton from "../components/Dbutton.vue";
   components: { FormItem, Dbutton },
 })
 export default class EditLabel extends Vue {
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag;
   }
   created() {
     const id = this.$route.params.id;
     this.$store.commit("fetchTags");
     this.$store.commit("setCurrentTag", id);
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace("/404");
     }
   }
   update(name: string) {
-    if (this.tag) {
-      this.$store.commit("updateTag", { id: this.tag.id, name: name });
+    if (this.currentTag) {
+      this.$store.commit("updateTag", { id: this.currentTag.id, name: name });
     }
   }
   remove() {
-    if (this.tag) {
-      this.$store.commit("removeTag", this.tag.id);
+    if (this.currentTag) {
+      this.$store.commit("removeTag", this.currentTag.id);
     }
   }
   goBack() {
