c6f0fee3e1c86c41b452c9343ed76b934339d39e
tai-xiaoDong <672236157@qq.com>
2022-07-12 06:12:03 +0800

    修复了很多小问题  设置了默认标签

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -9,6 +9,11 @@ export class TagHelper extends Vue {
             return window.alert("标签名不能为空！");
         }
         this.$store.commit("createTag", name);
+        if (this.$store.state.createTagError) {
+            if (this.$store.state.createTagError.message === 'tag name duplicated') {
+                window.alert('标签名重复了！')
+            }
+        }
     }
 }
 
